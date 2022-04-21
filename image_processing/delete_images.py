f95de76632a85009e2a6124a4f57d0f20b5bd41d
MasterD20 <voroninihor@gmail.com>
2022-04-21 23:38:29 +0300

    new: user-friendly output for delete folder command

--- image_processing/delete_images.py
+++ image_processing/delete_images.py
@@ -1,16 +1,25 @@
 import shutil
 
+from progress_bar import progress_bar
 from settings import Settings
 
 
 def delete_images() -> None:
+    print("Delete folders with images ...")
     image_folders = [
         Settings.get_temp_path(),
         Settings.get_save_screenshot_path(),
         Settings.get_save_processed_path(),
     ]
-    for image_folder in image_folders:
+    progress_bar(
+        0, len(image_folders), prefix="Progress:", suffix="Complete", length=50
+    )
+    for num, image_folder in enumerate(image_folders, start=1):
+        progress_bar(
+            num, len(image_folders), prefix="Progress:", suffix="Complete", length=50
+        )
         try:
             shutil.rmtree(image_folder)
         except FileNotFoundError:
             pass
+    print("All used folders removed.")
