0faf27e031c9d7d68b24689c65443fac302e4c48
Mary1311 <mary13lyashenko@gmail.com>
2022-04-29 14:07:26 +0200

    chg: added possibility to execute all commands & got list of window_id and window_name

--- main.py
+++ main.py
@@ -9,29 +9,44 @@ from window_controll.window_list import list_of_open_windows
 
 
 def main() -> None:
+    all_methods = False
+    if (
+        vars(args)[
+            "windows-list"
+            and "prepare-nicknames"
+            and "recognize-templates"
+            and "screenshot-generation"
+            and "clean"
+        ]
+        is False
+    ):
+        all_methods = True
+
     if args.load_settings:
-        Settings.load_from_string(args.load_settings)
+        print("Settings.load_from_string(args.load_settings)")
 
     if args.load_file:
-        Settings.load_from_file(args.load_file)
+        print("Settings.load_from_file(args.load_file)")
 
     if args.display_settings:
-        Settings.display_settings()
+        print("Settings.display_settings()")
 
-    if args.windows_list:
-        list_of_open_windows()
+    if args.windows_list or all_methods:
+        a = list_of_open_windows()
+        # print(a)
 
-    if args.screenshot_generation:
-        create_screenshots_of_nicknames(args.screenshot_generation)
+    if args.screenshot_generation or all_methods:
+        print("create_screenshots_of_nicknames(args.screenshot_generation)")
 
-    if args.prepare_nicknames:
-        prepare_nicknames()
+    if args.prepare_nicknames or all_methods:
+        print("prepare_nicknames()")
 
-    if args.recognize_templates:
-        recognize_from_templates()
+    if args.recognize_templates or all_methods:
+        print("recognize_from_templates()")
 
-    if args.clean:
-        clean_folders()
+    if args.clean or all_methods:
+        print("clean_folders()")
+        print("aaaaaa")
 
 
 if __name__ == "__main__":
@@ -84,6 +99,6 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--clean", action="store_true", help="Clean used folders")
 
     args = parser.parse_args()
-    config = vars(args)
+    # config = vars(args)
     # print(config)
     main()
--- window_controll/window_list.py
+++ window_controll/window_list.py
@@ -1,16 +1,19 @@
-from typing import Any
+from typing import Any, Dict, List
 
 import win32gui
 
 
-def winEnumHandler(hwnd: Any, ctx: Any) -> None:
+def winEnumHandler(hwnd: Any, all_windows: List[Dict[Any, Any]]) -> None:
     if win32gui.IsWindowVisible(hwnd):
         window_name = win32gui.GetWindowText(hwnd)
         if window_name:
+            all_windows.append({hwnd: window_name})
             print(f"{hwnd:10} -- {window_name}")
 
 
-def list_of_open_windows() -> None:
+def list_of_open_windows() -> list:
     print("List of open windows with their IDs")
     print("window_id \t window_name")
-    return win32gui.EnumWindows(winEnumHandler, None)
+    all_windows: list = []
+    win32gui.EnumWindows(winEnumHandler, all_windows)
+    return all_windows
