3975a52a5bcbfc5d6479b070800f30b1c8c5d3c1
Mary1311 <mary13lyashenko@gmail.com>
2022-05-12 23:00:18 +0200

    new: created tests as test_load_from_file_successful, test_load_from_file_incorect_type and test_load_from_file_incorect_key

--- test_csm.py
+++ test_csm.py
@@ -1,7 +1,8 @@
-import json
+import json, tempfile
 
 import pytest
 
+from testfixtures import TempDirectory
 from settings import Settings
 
 
@@ -23,3 +24,31 @@ def test_load_from_sting_incorect_key():
     settings_string = json.dumps({"page_count_non_existed": s.page_count + 1})
     with pytest.raises(AttributeError):
         s.load_from_string(settings_string)
+
+def test_load_from_file_successful():
+    s = Settings()
+    new_page_count = s.page_count + 1
+    tfile = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    json.dump({"page_count": new_page_count}, tfile)
+    tfile.flush()
+    filename = tfile.name
+    s.load_from_file(filename)
+    assert s.page_count == new_page_count
+
+def test_load_from_file_incorect_type():
+    s = Settings()
+    tfile = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    json.dump({"page_count": str(s.page_count + 1)}, tfile)
+    tfile.flush()
+    filename = tfile.name
+    with pytest.raises(TypeError):
+        s.load_from_file(filename)
+
+def test_load_from_file_incorect_key():
+    s = Settings()
+    tfile = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    json.dump({"page_count_non_existed": s.page_count + 1}, tfile)
+    tfile.flush()
+    filename = tfile.name
+    with pytest.raises(AttributeError):
+        s.load_from_file(filename)
