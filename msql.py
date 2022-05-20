9864ca24bd337e86eb65e1f58b83eeb581006bf3
Mary1311 <mary13lyashenko@gmail.com>
2022-05-20 17:37:13 +0200

    ch: added methods: start_or_restart_service, start_service, stop_service, restart_service, status_service

--- msql.py
+++ msql.py
@@ -1,11 +1,44 @@
 from typing import Dict
 
 import pymysql
+import win32serviceutil
 
 from settings import Settings
 
 
+def start_service(service: str) -> None:
+    print("Start MySQL service")
+    return win32serviceutil.StartService(service)
+
+
+def stop_service(service: str) -> None:
+    print("Stop MySQL service")
+    return win32serviceutil.StopService(service)
+
+
+def restart_service(service: str) -> None:
+    print("Restart MySQL service")
+    return win32serviceutil.RestartService(service)
+
+
+def status_service(service: str) -> tuple:
+    return win32serviceutil.QueryServiceStatus(service)
+
+
+def start_or_restart_service(service: str) -> None:
+    if not status_service(service):
+        print("MySQL service is NOT installed")
+        raise Exception
+    if status_service(service)[1] != Settings.service_status_running:
+        print("MySQL is off")
+        return start_service(service)
+    if status_service(service)[1] == Settings.service_status_running:
+        print("MySQL is running now")
+        return restart_service(service)
+
+
 def select_money() -> Dict[int, float]:
+    start_or_restart_service(Settings.service_name)
     result = dict()
     try:
         connection = pymysql.connect(
